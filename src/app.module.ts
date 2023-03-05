d25c5372dd4735b30016e812621185120827bb1a
TMWAG <semyon.valyaev@gmail.com>
2023-03-06 00:15:19 +0300

    подключен модуль для работы с .env файлами и модуль typeorm

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,18 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import CONNECTION from './config/db.config';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    ConfigModule.forRoot(),
+    TypeOrmModule.forRoot({
+      ...CONNECTION,
+      autoLoadEntities: true,
+      synchronize: Boolean(process.env.DB_SYNC),
+    }),
+  ],
+  controllers: [],
+  providers: [],
 })
 export class AppModule {}
