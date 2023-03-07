bc9dad870aa8c1bf96feaf635d5349aea528b1e5
TMWAG <semyon.valyaev@gmail.com>
2023-03-07 23:31:08 +0300

    объект с текстом ощибок валидации dto

---
+++ src/errors/messages/dto-validation.messages.ts
@@ -0,0 +1,25 @@
+export const dtoValidationError = {
+  type: {
+    string: 'Должен быть строкой',
+    int: 'Должен быть целым числом',
+    bool: 'Должен быть булевого типа',
+    email: 'Должен быть адресом электронной почты',
+    phone: 'Должен быть номером телефона',
+    positive: 'Не может быть отрицательным',
+    uuidV4: 'Должен быть uuid v4',
+  },
+  length: {
+    min: (min: number): string => {
+      return `Не может быть меньше ${min}`;
+    },
+    max: (max: number): string => {
+      return `Не может быть больше ${max}`;
+    },
+    length: (min: number, max: number): string => {
+      return `Должен быть длинной ${min}-${max} символа`;
+    },
+    in: (from: number, to: number): string => {
+      return `Должно быть в диапазоне от ${from} до ${to}`;
+    },
+  },
+};
