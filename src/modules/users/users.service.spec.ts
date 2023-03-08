a62d5ed80fbebf65f2c03a1ea3496145e7460f53
TMWAG <semyon.valyaev@gmail.com>
2023-03-08 21:28:49 +0300

    реализован метод create

--- src/modules/users/users.service.spec.ts
+++ src/modules/users/users.service.spec.ts
@@ -1,6 +1,7 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { getRepositoryToken } from '@nestjs/typeorm';
 import { randomUUID } from 'crypto';
+import { CreateUserDto } from './dto/create-user.dto';
 import { User, UserRole } from './entities/user.db.entity';
 import { UsersService } from './users.service';
 
@@ -14,7 +15,16 @@ describe('UsersService', () => {
         {
           provide: getRepositoryToken(User),
           useValue: {
-            create: jest.fn(),
+            create: jest.fn((createUserDto: CreateUserDto) => {
+              return {
+                id: Date.now(),
+                ...createUserDto,
+                role: UserRole.CUSTOMER,
+                vkLink: null,
+                tgLink: null,
+                token: randomUUID(),
+              };
+            }),
           },
         },
       ],
@@ -25,5 +35,25 @@ describe('UsersService', () => {
 
   it('should be defined', () => {
     expect(service).toBeDefined();
+  });
+
+  describe('create', () => {
+    const createUserDto: CreateUserDto = {
+      name: 'Никита',
+      surname: 'Нечаев',
+      email: 'example@mail.me',
+      phone: '+79991112233',
+      password: 'RandomPass111',
+    };
+    it('should return user', async () => {
+      expect(service.create(createUserDto)).toEqual({
+        id: expect.any(Number),
+        ...createUserDto,
+        role: UserRole.CUSTOMER,
+        vkLink: null,
+        tgLink: null,
+        token: expect.any(String),
+      });
+    });
   });
 });
--- src/modules/users/users.service.ts
+++ src/modules/users/users.service.ts
@@ -1,11 +1,17 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
+import { User } from './entities/user.db.entity';
 
 @Injectable()
 export class UsersService {
+  constructor(
+    @InjectRepository(User) private usersRepository: Repository<User>,
+  ) {}
   create(createUserDto: CreateUserDto) {
-    return 'This action adds a new user';
+    return this.usersRepository.create(createUserDto);
   }
 
   findAll() {
